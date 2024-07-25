e2a2741eafdd85eb069943c655a27b0235492dff
Alec Croue <alec.croue@gmail.com>
2024-07-25 11:28:30 +0200

    added vscode install

--- init_inception.sh
+++ init_inception.sh
@@ -5,7 +5,8 @@ echo "$USER  ALL=(ALL) NOPASSWD: ALL" >> /etc/sudo
 exit
 
 # install utils
-apt install curl htop git
+sudo apt update
+sudo apt install curl htop git
 
 # install docker-compose
 sudo curl -SL https://github.com/docker/compose/releases/download/v2.29.1/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
@@ -17,3 +18,10 @@ sudo mkdir -p /mnt/cdrom
 sudo mount /dev/cdrom /mnt/cdrom
 sudo /mnt/cdrom/VBoxLinuxAdditions.run
 sudo reboot
+
+# install vscode
+sudo apt install software-properties-common apt-transport-https wget -y
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt install code
+code --version
