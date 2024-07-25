6d95a730dccd0d6a2bbf831d1c8454dd35767c6f
Alec Croue <alec.croue@gmail.com>
2024-07-25 11:17:27 +0200

    changed init

--- init_inception.sh
+++ init_inception.sh
@@ -1,5 +1,19 @@
+# set user as sudo
+su
+adduser $USER sudo
+echo "$USER  ALL=(ALL) NOPASSWD: ALL" >> /etc/sudo
+exit
 
+# install utils
 apt install curl htop git
+
+# install docker-compose
 sudo curl -SL https://github.com/docker/compose/releases/download/v2.29.1/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose;
 
+# install guest additions
+sudo apt install -y build-essential dkms linux-headers-$(uname -r)
+sudo mkdir -p /mnt/cdrom
+sudo mount /dev/cdrom /mnt/cdrom
+sudo /mnt/cdrom/VBoxLinuxAdditions.run
+sudo reboot
