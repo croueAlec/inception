89e6899c6e51af67d955d5b50120560e72f45f8e
acroue <acroue@inception.myguest.virtualbox.org>
2024-08-22 23:39:52 +0200

    maria db works, maybe idk

---
+++ srcs/requirements/mariadb/mariadb_start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};" > /etc/mysql/init.sql
+echo "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';" >> /etc/mysql/init.sql
+echo "GRANT ALL PRIVILEGES ON *.* TO '${SQL_USER}'@'%' WITH GRANT OPTION;" >> /etc/mysql/init.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+echo "FLUSH PRIVILEGES;" >> /etc/mysql/init.sql
+
+mysqld
