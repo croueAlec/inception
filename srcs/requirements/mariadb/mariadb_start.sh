f19d36e94cda671fd0c0517debb7bad1a1c4dd11
acroue <acroue@inception.myguest.virtualbox.org>
2024-09-02 15:59:03 +0200

    fix password issue with root in mariadb

--- srcs/requirements/mariadb/mariadb_start.sh
+++ srcs/requirements/mariadb/mariadb_start.sh
@@ -3,7 +3,7 @@
 echo "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};" > /etc/mysql/init.sql
 echo "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';" >> /etc/mysql/init.sql
 echo "GRANT ALL PRIVILEGES ON *.* TO '${SQL_USER}'@'%' WITH GRANT OPTION;" >> /etc/mysql/init.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';" >> /etc/mysql/init.sql
 echo "FLUSH PRIVILEGES;" >> /etc/mysql/init.sql
 
 exec mysqld_safe
